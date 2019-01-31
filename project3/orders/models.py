2a22ce81b6b4c0b99ce9a9d13d401de5ff31d5ca
ravindrareddy2 <ravindrareddy1729@gmail.com>
2019-01-31 22:17:19 +0530

    created models.py

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+#to create different pizza models
+class type_of_pizza(models.model):
+    name = models.CharField(max_length=64)
+
+    def __str__ (self):
+        return f"{self.name}"
+
+#to create regular pizza model
+class regular_pizza(models.model):
+    name = models.ForeignKey(type_of_pizza, on_delete=CASCADE)
+    small = models.DecimalField(max_digits=4,decimal_places=2)
+    large = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}-{self.small}-{self.large}"
+
+#to create sicilian pizza
+class sicilian_pizza(models.model):
+    name = models.ForeignKey(type_of_pizza, on_delete=CASCADE)
+    small = models.DecimalField(max_digits=4, decimal_places=2)
+    large = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}-{self.small}-{self.large}"
+
+
+
